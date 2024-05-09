beb30a56345c60b021302929e5b59cb1dbd735c8
tesla <victorianodev@gmail.com>
2024-05-09 10:52:16 -0600

    mensaje de presentacion

--- main.cpp
+++ main.cpp
@@ -2,20 +2,24 @@
 #include <string>
 #include <vector>
 #include "colors.h"
+
 using namespace std;
 
+int main()
+{
 
-int main() {
-  // Ejemplo de como usar el color en terminal
-  /*
-  cout << RED << BG_YELLOW << "Hola soy una libreria" << RESET << endl;
-  cout << GREEN << "Hola soy una libreria" << RESET << endl;
-  */
-  
-  cout << RED << BG_YELLOW << "Hola soy una libreria" << RESET << endl;
-  cout << GREEN << "Hola soy una libreria" << RESET << endl;
+  cout << endl
+       << GREEN << "Sistema de Gestion de Libreria UVG" << RESET << endl;
+  cout << endl
+       << "Bienvenidos a la biblioteca UVG," << endl
+       << "en donde encontraras los mejores libros." << endl
+       << "Actualizados 2024" << endl;
+
+  cout << endl
+       << "Que deseas realizar en el Sistema de Biblioteca: " << endl
+       << endl;
   system("pause");
-  system("cls");
-  cout << "Hola";
-	return 0;
+
+
+  return 0;
 }
