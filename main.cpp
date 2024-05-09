68e18944facb13371908a8f2384dec27c099ea0d
JhonArl123 <naujralios@gmail.com>
2024-05-09 11:57:37 -0600

    switch

--- main.cpp
+++ main.cpp
@@ -8,16 +8,23 @@ using namespace std;
 int main()
 {
 
+	int options;
   cout << endl
        << GREEN << "Sistema de Gestion de Libreria UVG" << RESET << endl;
   cout << endl
        << "Bienvenidos a la biblioteca UVG," << endl
        << "en donde encontraras los mejores libros." << endl
        << "Actualizados 2024" << endl;
+       
+       system("pause");
+ 	system("cls");
 
-  cout << endl
-       << "Que deseas realizar en el Sistema de Biblioteca: " << endl
-       << endl;
+  cout << endl;
+      
+	 // do {
+	  
+	   cout<< "Que deseas realizar en el Sistema de Biblioteca: " << endl;
+       cout<< endl;
        cout<<"=========="<<endl;
        cout<<"Biblioteca"<<endl;
        cout<<"=========="<<endl;
@@ -30,8 +37,34 @@ int main()
        cout<<"4. Descripcion"<<endl;
        cout<<"5. Salir"<<endl;
        
-  system("pause");
-
-
+       
+	  //} while();
+    
+  	cout<<"ingrese la opcion"<<endl;
+  	cin>>options;
+  
+  	
+	switch(options){
+		
+		case 1:
+			cout<<"Ingresar libros: "<<endl;
+			break;
+			
+		case 2:
+			cout<<"Buscar libro: "<<endl;
+			break;
+		case 3:
+			cout<<"Actualizar informacion: "<<endl;
+			break;
+		case 4:
+			cout<<"Descripcion: "<<endl;
+			break;
+		case 5:
+			cout<<" cerrar sesion: "<<endl;
+			break;	
+	
+		
+		}
+		
   return 0;
 }
