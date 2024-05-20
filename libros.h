e97cb7acce8dee56de6ba7551e6d6fcc2d52192c
JhonArl123 <naujralios@gmail.com>
2024-05-20 17:17:32 -0600

    Create libros.h
    tercer okey

---
+++ libros.h
@@ -0,0 +1,16 @@
+// tare para Juan -- Inicio
+#ifndef LIBROS_H
+#define LIBROS_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Libro {
+    string titulo;
+    string autor;
+    string genero;
+    int anio_publicacion;
+    string estatus;
+};
