080ce63e6363767e5c3147585ca8c4e9c2ea80aa
Andre1s2 <168664802+Andre1s2@users.noreply.github.com>
2024-05-20 16:24:02 -0600

    Update main.cpp (#19)

--- main.cpp
+++ main.cpp
@@ -1,3 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "libros.h"
+
+using namespace std;
+
+// Variables Globales
+string h = "\t";
+string v = "\v";
+
+
+// Funciones
+void mostrar(const vector<Libro>& libros) {
+    for (int i = 0; i < libros.size(); i++) {
+        cout << endl << h << i << ". " << libros[i].titulo << h
+             << libros[i].autor << h << libros[i].genero << h << libros[i].anio_publicacion << h << libros[i].estatus << endl;
+    }
+}
+
 int main() {
     vector<Libro> libros;
     libros.push_back({"Eloquent Javascript", "Marijin Haverbeke", "Programacion", 2024, "Disponible"});
